55b8b2fa778c3c70787df30e0e90ddd6f87a4cb9
VieetBubbles <537@holbertonschool.com>
2019-04-29 22:10:32 +0000

    Updated the comments to be more detailed

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -3,7 +3,7 @@
 /**
  * check_cycle - function in C that checks if a singly
  * linked list has a cycle in it.
- * @list: the linked list
+ * @list: a pointer to the head of the linked list
  *
  * Return: 0 if there is no cycle, 1 if there is a cycle
  */
@@ -19,10 +19,10 @@ int check_cycle(listint_t *list)
 	/* Traverse linked list using two pointers */
 	while(slow_p && fast_p && fast_p->next)
 	{
-		/* Move one pointer by one */
+		/* Move one pointer by one node */
 		slow_p = slow_p->next;
 
-		/* Move other pointer by two */
+		/* Move other pointer by two nodes */
 		fast_p = fast_p->next->next;
 
 		/*  If these pointers meet at same node then there is a loop */
