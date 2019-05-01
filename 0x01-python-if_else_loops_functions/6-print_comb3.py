a4f58ef59ddaf45cd876747d9935fdc80dd63b0f
VieetBubbles <537@holbertonschool.com>
2019-05-01 01:00:30 +0000

    Fixed trailing space error with the end=,

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for fl in range(0, 8):
     for ld in range(fl + 1, 10):
-        print("{:d}{:d}".format(fl, ld), end =", ")
+        print("{:d}{:d}".format(fl, ld), end=", ")
 print("89")
