17b95c9392a985e563d49faa0913b362c2036c3f
VieetBubbles <537@holbertonschool.com>
2019-05-03 04:03:20 +0000

    CChanged the for loop to be a list comprehension

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    mylist = []
-    for _ in dir(hidden_4):
-        if not _.startswith('__'):
-            mylist.append(_)
 
-    for string in mylist:
+    # mylist = [_ for _ in dir(hidden_4) if not _.startswith('__')]
+    # for _ in dir(hidden_4):
+    # if not _.startswith('__'):
+    # mylist.append(_)
+
+    for string in [_ for _ in dir(hidden_4) if not _.startswith('__')]:
         print("{}".format(string))
