35ad23a13034ca3cda8744e5163b30c0edb9eb86
VieetBubbles <537@holbertonschool.com>
2019-05-08 06:27:51 +0000

    Replaced previous method with the concate method of tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    a, b, c, d = 0, 0, 0, 0
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
 
-    if len(tuple_a) == 1:
-        a = tuple_a[0]
+    # tuple_a = (1, 89, 0, 0)
+    # tuple_b = (88, 11, 0, 0)
 
-    elif len(tuple_a) >= 2:
-        a, b = tuple_a[:2]
+    # tuple_b = (1, 0, 0)
+    # tuple_b = (0, 0)
 
-    if len(tuple_b) == 1:
-        c = tuple_b[:0]
+    new = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
 
-    elif len(tuble_b) >= 2:
-        c, d = tuple_b[:2]
-    return a + c, b + d
+    return new
