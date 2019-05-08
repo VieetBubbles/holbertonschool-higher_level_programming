88cefcc2c8f7156c5dac82755048fbba2610461f
VieetBubbles <537@holbertonschool.com>
2019-05-08 06:05:45 +0000

    Added an extra return for the edge case for None

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == "":
-        return None
+    if sentence == "":
+        return (len(sentence), None)
 
     return (len(sentence), sentence[0])
