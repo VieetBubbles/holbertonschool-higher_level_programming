4cce763d2778f7fe20e0b74897c3c8fc5a712b62
VieetBubbles <537@holbertonschool.com>
2019-05-09 10:06:05 +0000

    Python script that replaces or adds key/value in a dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    if key in a_dictionary:
+        for x in a_dictionary:
+            if x == key:
+                a_dictionary[x] = value
+                sorted(a_dictionary)
+                return a_dictionary
+
+    if  key not in a_dictionary:
+        a_dictionary[key] = value
+        sorted(a_dictionary)
+        return a_dictionary
