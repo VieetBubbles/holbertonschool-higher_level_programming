7e26811456210e8bce440f5b745b108f7558fd0e
VieetBubbles <537@holbertonschool.com>
2019-05-09 20:22:34 +0000

    Chnaged the syntax of the return statements

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary:
+        maximum = max(a_dictionary, key = a_dictionary.get)
+        return maximum
+    else:
         return None
-    maximum = max(a_dictionary, key = a_dictionary.get)
-    return maximum
