3afb46d782308d95bf565b7357964796a60d254f
VieetBubbles <537@holbertonschool.com>
2019-05-09 22:00:51 +0000

    Added in an extra if statement to handle the decrementing numerals

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,15 +4,22 @@ def roman_to_int(roman_string):
                'L': 50, 'X': 10, 'V': 5, 'I': 1}
 
     sum = 0
+    prev_rn = 0
 
     if not isinstance(roman_string, str):
         return 0
 
-    if roman_string is None:
+    if roman_string is  None:
         return 0
 
     for _ in roman_string:
         if _ in my_dict:
-            sum += my_dict[_]
-
+            # Handles highest to lowest order
+            if my_dict[_] <= prev_rn:
+                sum += my_dict[_]
+                prev_rn = my_dict[_]
+            # Handles lowest to highest order and single r_numerals
+            else:
+                sum += my_dict[_] - prev_rn * 2
+                prev_rn = my_dict[_]
     return sum
