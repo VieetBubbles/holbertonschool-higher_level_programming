5c385e327be74d6ff8b50193823a831f8ff459ea
VieetBubbles <537@holbertonschool.com>
2019-05-21 01:44:23 +0000

    Removed unnecessary semicolon from count.

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for _ in range(x):
             print("{}".format(my_list[_]), end="")
-            count = count + 1;
+            count = count + 1
     except:
         pass
 
