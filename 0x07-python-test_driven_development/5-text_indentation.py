4f05464dfd174b597d22ca5f6f99c1423e3642d8
VieetBubbles <537@holbertonschool.com>
2019-05-25 05:13:41 +0000

    Python script that prints text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+The text indentation module
+"""
+def text_indentation(text):
+    """
+    Function that prints a text with 2 new lines after each of these characters:
+    ., ? and :
+    Args:
+        text: the string that is to be broken down
+    """
+    new_sentence = ""
+    after_ns_space = True
+
+    if type(text) != str:
+        raise TypeError("text must be a string")
+
+    for c in text.strip():
+
+        if c is " " and after_ns_space is True:
+            pass
+
+        elif c is "." or c is "?" or c is ":":
+            new_sentence += c + "\n\n"
+            after_ns_space = True
+
+        else:
+            new_sentence += c
+            after_ns_space = False
+
+    print(new_sentence, end="")
