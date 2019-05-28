23784300b02c99c3938e5cf9d61cbdea1f925de6
VieetBubbles <537@holbertonschool.com>
2019-05-28 09:04:16 +0000

    Python script that defines a rectangle classes width and height

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""
+The module that defines a Rectangle class
+"""
+
+
+class Rectangle:
+    """
+    The Reactangle class
+    """
+    def __init__(self, width=0, height=0):
+        """
+        The initialization of an insatnce of Rectangle
+        Args:
+            width: The width of the rectangle object
+            height: The height of the rectangle object
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """
+        method that gets/retrieves the width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        method that sets the width to a certain value
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        method that gets/retrieves the height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        method that sets the height to a certain value
+        """
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
