bc0f31a2066d966780ea11e36e33d0c1bf820bf5
VieetBubbles <537@holbertonschool.com>
2019-06-07 07:00:01 +0000

    Wrote a class student that has a new public method that replaces all attributes of the Student instance.

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+The 13-student module.
+"""
+
+
+class Student:
+    """The class Student"""
+    def __init__(self, first_name, last_name, age):
+        """initialize the object"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        method that retrieves a dictionary representation of a Student instance
+        Args:
+            attrs - a list of strings or not
+        """
+        if type(attrs) is list and all(type(st) is str for st in attrs):
+            dictionary = {}
+            for key, value in self.__dict__.items():
+                if key in attrs:
+                    dictionary[key] = value
+            return dictionary
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        Public method that replaces all attributes of the Student instance
+        """
+        self.__dict__ = json
