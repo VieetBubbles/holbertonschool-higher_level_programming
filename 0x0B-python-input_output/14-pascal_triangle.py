a5003389de0c0b471d45dd790d63954eb9adb9c8
VieetBubbles <537@holbertonschool.com>
2019-06-08 06:32:10 +0000

    Python scrip that creates a pascal triangle.

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+The 14-pascal_triangle.py module.
+"""
+
+
+def pascal_triangle(n):
+    """
+    function that returns a list of lists of integers representing
+    the Pascal’s triangle of n
+
+    Args:
+        n - an integer
+    Return:
+        a list of lists of integers representing
+        the Pascal’s triangle of n
+    """
+    empty = []
+
+    if n <= 0:
+        return empty
+
+    pascal_t = []
+    row = []
+
+    for number in range(n):
+        row = [1]
+        if number > 0:
+            for j in range(number):
+                row.append(sum(pascal_t[-1][j:j + 2]))
+        pascal_t.append(row)
+    return pascal_t
