45585a01b67951bb960db17175632542b9cc98af
VieetBubbles <537@holbertonschool.com>
2019-06-12 08:20:03 +0000

    Added static method from_json_string  that returns the list of the JSON string representation json_string.

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -50,3 +50,14 @@ class Base:
 
         with open(file_name, 'w') as f:
             f.write(cls.to_json_string(new_ob_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        static method  that returns the list of
+        the JSON string representation json_string.
+        """
+        if json_string is None or len(json_string) is 0:
+            return []
+        else:
+            return json.loads(json_string)
