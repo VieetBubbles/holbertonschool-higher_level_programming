e83381b770bae32cd08538c678eae2d9e450c5a6
VieetBubbles <537@holbertonschool.com>
2019-06-12 06:44:21 +0000

    Added the public method update to the square class and added more documentation,

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,6 +6,7 @@ The square module.
 
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
     """
     The Square class that inherits from the Rectangle class.
@@ -18,18 +19,46 @@ class Square(Rectangle):
 
     def __str__(self):
         """
+        Overloading the method with a custom string
         """
-        return ("[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id, self.x, self.y, self.size))
+        return ("[Square] ({:d}) {:d}/{:d} - {:d}"
+                .format(self.id, self.x, self.y, self.size))
 
     @property
     def size(self):
         """
+        method to retreive the size
         """
         return self.width
 
     @size.setter
     def size(self, value):
         """
+        setter method to set the size and reset the width and height
         """
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """
+        public method that that assigns attributes.
+        """
+        if len(args):
+            for index, value in enumerate(args):
+                if index is 0:
+                    self.id = value
+                if index is 1:
+                    self.size = value
+                if index is 2:
+                    self.x = value
+                if index is 3:
+                    self.y = value
+        else:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "size" in kwargs:
+                self.size = kwargs["size"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
