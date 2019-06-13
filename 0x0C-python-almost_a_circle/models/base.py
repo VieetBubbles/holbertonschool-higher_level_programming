1d15f65855844649ef747edf394beccdeebecde7
VieetBubbles <537@holbertonschool.com>
2019-06-13 01:07:30 +0000

    Removed extra parentheses from the ti_json_string static method.

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -33,7 +33,7 @@ class Base:
         if list_dictionaries is None:
             return "[]"
         else:
-            return (json.dumps(list_dictionaries))
+            return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
