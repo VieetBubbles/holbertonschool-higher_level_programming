aa65336fa147643b7cfd4d97a1310b492f7eb5b0
VieetBubbles <537@holbertonschool.com>
2019-07-15 22:28:19 +0000

    Added a WHERE command

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,3 @@
 -- Script that displays the number of records with id = 89 in the table first_table
 -- Using the command SELECT, FROM , and COUNT
-SELECT COUNT(id) FROM first_table;
+SELECT COUNT(*) FROM first_table WHERE id = 89;
