2047bb2ca68c46e066de74922c89a95deb6dd6bf
VieetBubbles <537@holbertonschool.com>
2019-07-16 01:47:52 +0000

    Added the COLLATE

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,4 +2,4 @@
 -- b,b
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE hbtn_0c_0;
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
