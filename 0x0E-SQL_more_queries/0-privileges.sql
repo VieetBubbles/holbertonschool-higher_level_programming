442162d4ae0408d7ac70a075bd123ccf2a4d4a7c
VieetBubbles <537@holbertonschool.com>
2019-07-16 22:03:23 +0000

    MySQL script that grants user permision

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- Script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server
+-- Using SHOW FOR & GRANTS
+SHOW GRANTS FOR user user_0d_1;
+SHOW GRANTS FOR user user_0d_2;
