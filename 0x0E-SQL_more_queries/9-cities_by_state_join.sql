177fc3ad53670c1e9d88a9225ef3c329df01146f
VieetBubbles <537@holbertonschool.com>
2019-07-17 00:57:16 +0000

    MySQL script that use JOIN

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,4 @@
+-- Script that lists all cities contained in the database hbtn_0d_usa
+-- You can use only one SELECT statement
+SELECT cities.id, cities.name, states.name FROM cities
+JOIN states ON cities.state_id = states.id;
