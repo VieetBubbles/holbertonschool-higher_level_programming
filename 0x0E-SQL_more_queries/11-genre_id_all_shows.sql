81b154136437809568bc6739a7fd16bfb9b67716
VieetBubbles <537@holbertonschool.com>
2019-07-17 02:20:02 +0000

    Swapped JOIN with RIGHT JOIN

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 -- Script that lists all shows contained in the database hbtn_0d_tvshows
 -- You can use only one SELECT statement
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
-JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+RIGHT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
