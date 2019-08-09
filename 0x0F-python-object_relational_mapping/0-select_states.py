84098bef327e4b8eef59308b6d74b31af9534aba
VieetBubbles <537@holbertonschool.com>
2019-08-09 19:54:08 +0000

    fixed pep8 errors

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
 # Write a script that lists all states from the database hbtn_0e_0_usa
 
-import MySQLdb
-
-db = MySQLdb.connect(host="localhost", port=3306,      # your host, usually localhost
-                     user="root",                      # your username
-                     passwd="root",                    # your password
-                     db="hbtn_0e_0_usa",               # name of the data base
-                     charset="utf8")
-
-# you must create a Cursor object. It will let
-#  you execute all the queries you need
-cur = db.cursor()
-
-cur.execute("SELECT * FROM states ORDER BY id ASC") # HERE I have to know SQL to grab all states in my database
-
-# print all the rows in the table
-query_rows = cur.fetchall()
-
 if __name__ == '__main__':
+    import MySQLdb
+
+    db = MySQLdb.connect(host="localhost", port=3306,      # your host
+                         user="root",                      # your username
+                         passwd="root",                    # your password
+                         db="hbtn_0e_0_usa",               # name of database
+                         charset="utf8")
+
+    # you must create a Cursor object. It will let
+    #  you execute all the queries you need
+    cur = db.cursor()
+
+    # HERE I have to know SQL to grab all states in my database
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # print all the rows in the table
+    query_rows = cur.fetchall()
+
     for row in query_rows:
         print(row)
 
-cur.close()
-db.close()
+    cur.close()
+    db.close()
