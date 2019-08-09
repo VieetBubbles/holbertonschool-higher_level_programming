07bff95711da0833b5b0171cce5c54317a55b78d
VieetBubbles <537@holbertonschool.com>
2019-08-09 19:56:05 +0000

    Chnaged the 2nd comment line

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Write a script that lists all states from the database hbtn_0e_0_usa
+# Write a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
 
 if __name__ == '__main__':
     import MySQLdb
