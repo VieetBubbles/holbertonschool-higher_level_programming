edb6d72b7e855463d29b15a924c5e8980a607c36
VieetBubbles <537@holbertonschool.com>
2019-08-11 22:42:54 +0000

    Used format style instead of printing the variable

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     query_rows = cur.fetchall()
 
     for row in query_rows:
-        print(row)
+        print("{}, {}".format(row[0], row[1]))
 
     cur.close()
     db.close()
