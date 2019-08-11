8eed4aeddb4e0a9d6897556a9ee91d626af0713f
VieetBubbles <537@holbertonschool.com>
2019-08-11 22:24:14 +0000

    Fixed placement of previous commit line

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -7,9 +7,10 @@ from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-state_name = sys.argv[4]
-
 if __name__ == '__main__':
+
+    state_name = sys.argv[4]
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format("root", "root", sys.argv[3]),
                            pool_pre_ping=True)
