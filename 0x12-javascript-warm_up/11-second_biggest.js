2ab05650ccb06d7d0ef955b3c7ea197b06fd9834
VieetBubbles <537@holbertonschool.com>
2019-09-18 03:04:38 +0000

    Added else statement to fix undefined error

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 if (process.argv.length < 4) {
   console.log('0');
+} else {
+  const array = process.argv.slice(2);
+  array.sort();
+  console.log(array.slice(-2)[0]);
 }
-const array = process.argv.slice(2);
-array.sort();
-console.log(array.slice(-2)[0]);
