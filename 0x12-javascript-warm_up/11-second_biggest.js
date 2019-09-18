3e0ad106a8a9db703e9bd6f4214bf37b1a0ede12
VieetBubbles <537@holbertonschool.com>
2019-09-18 03:11:27 +0000

    changed sort syntax

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,6 @@ if (process.argv.length < 4) {
   console.log('0');
 } else {
   const array = process.argv.slice(2);
-  array.sort();
+  array.sort((a, b) => a - b);
   console.log(array.slice(-2)[0]);
 }
