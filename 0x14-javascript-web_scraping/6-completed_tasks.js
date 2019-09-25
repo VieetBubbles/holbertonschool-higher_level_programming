0bf92e566d3452280aabed31787ca225f4e79c4f
VieetBubbles <537@holbertonschool.com>
2019-09-25 05:30:23 +0000

    Swapped the if statements' position

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -12,13 +12,13 @@ request.get(process.argv[2], function (err, res, body) {
     for (let i = 1; i < list.length; i++) {
       const key = list[i].userId;
       if (list[i].completed === true) {
-        // increase value whenever same id and complete is true shows up
-        if (newDict[key]) {
-          newDict[key] += 1;
-
         // if new dictionary at id is empty then fill in 1st value as 1.
-        } else {
+        if (!newDict[key]) {
           newDict[key] = 1;
+
+        // increase value whenever same id and complete is true shows up
+        } else {
+          newDict[key] += 1;
         }
       }
     }
